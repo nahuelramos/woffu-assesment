94da3ac86b509d1206319c26c8716b31666e0008
Nahuel Ramos <nahuelramos.nr@gmail.com>
2020-01-15 20:36:19 +0100

    Nahuel - master - add angular material and add table users from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core';
 import { UsersModule } from './users';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { UsersModule } from './users';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    UsersModule
+    UsersModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTableModule, MatPaginatorModule } from '@angular/material';
 
 import { UsersComponent } from './users.component';
 import { UsersTableComponent } from './components';
 import { UserService } from './services';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [
     UserService
